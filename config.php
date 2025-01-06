573dbd6f58f019107c622f0260326480d4864ff6
JAIMESKO1 <146107805+JAIMESKO1@users.noreply.github.com>
2025-01-06 02:05:09 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('CLIENT_ID', 'votre_client_id'); // Remplacez par l'ID du client
+define('CLIENT_SECRET', 'votre_client_secret'); // Remplacez par le secret du client
+define('REDIRECT_URI', 'http://localhost/callback.php'); // Remplacez par votre URI de redirection
+define('DISCORD_API', 'https://discord.com/api');
+?>
